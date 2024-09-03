60ebc253670060050a4b219d7f71f143b5bf21e6
Dean Ellis <dellis1972@googlemail.com>
2024-09-03 14:29:06 +0100

    Update the windows rids

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -13,11 +13,11 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     {
         var buildWorkingDir = "openal-soft/build_windows";
         context.CreateDirectory(buildWorkingDir);
-        context.CreateDirectory($"{context.ArtifactsDir}/windows-x64/");
+        context.CreateDirectory($"{context.ArtifactsDir}/win-x64/");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF .." });
         //context.ReplaceTextInFiles("assimp/code/assimp.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         //context.ReplaceTextInFiles("assimp/contrib/zlib/zlibstatic.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
-        context.CopyFile(@"openal-soft/build_windows/Release/OpenAL32.dll", $"{context.ArtifactsDir}/windows-x64/openal.dll");
+        context.CopyFile(@"openal-soft/build_windows/Release/OpenAL32.dll", $"{context.ArtifactsDir}/win-x64/openal.dll");
     }
 }
