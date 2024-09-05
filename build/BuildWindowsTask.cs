01b2c894f913e2637286a5195b9d79f9dc41c698
Dean Ellis <dellis1972@googlemail.com>
2024-09-05 15:28:27 +0100

    Try to statically link windows

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -14,14 +14,14 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         var buildWorkingDir = "openal-soft/build_windows_x64";
         context.CreateDirectory(buildWorkingDir);
         context.CreateDirectory($"{context.ArtifactsDir}/win-x64/");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -A x64 .." });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -DFORCE_STATIC_VCRT=ON -A x64 .." });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         context.CopyFile(@"openal-soft/build_windows_x64/Release/OpenAL32.dll", $"{context.ArtifactsDir}/win-x64/openal.dll");
 
         buildWorkingDir = "openal-soft/build_windows_arm64";
         context.CreateDirectory(buildWorkingDir);
         context.CreateDirectory($"{context.ArtifactsDir}/win-arm64/");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -A ARM64 .." });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -DFORCE_STATIC_VCRT=ON -A ARM64 .." });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         context.CopyFile(@"openal-soft/build_windows_arm64/Release/OpenAL32.dll", $"{context.ArtifactsDir}/win-arm64/openal.dll");
     }
