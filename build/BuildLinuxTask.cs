bd1c7a58d323613d0d69f7ce755deebd194d026d
Dean Ellis <dellis1972@googlemail.com>
2024-11-26 23:39:37 +0000

    Try stripping the binaries to reduce their size (#4)
    * Try stripping the binaries to reduce their size
    
    * Wrong order
    
    * Disable Pipewire for Android

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -16,6 +16,7 @@ public sealed class BuildLinuxTask : FrostingTask<BuildContext>
         context.CreateDirectory($"{context.ArtifactsDir}/linux-x64/");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -DALSOFT_BACKEND_SNDIO=OFF -DCMAKE_BUILD_TYPE=Release .." });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
+        context.StartProcess ("strip", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"--strip-all libopenal.so"});
         context.CopyFile($"{buildWorkingDir}/libopenal.so", $"{context.ArtifactsDir}/linux-x64/libopenal.so");
         BuildAndroid (context, "arm64-v8a", "android-arm64", "23");
         BuildAndroid (context, "armeabi-v7a", "android-arm", "23");
@@ -26,11 +27,13 @@ public sealed class BuildLinuxTask : FrostingTask<BuildContext>
     void BuildAndroid (BuildContext context, string arch, string rid, string minNdk)
     {
         var ndk = System.Environment.GetEnvironmentVariable ("ANDROID_NDK_HOME");
+        var strip = System.IO.Path.Combine (ndk ?? string.Empty, "toolchains", "llvm", "prebuilt", "linux-x86_64","bin","llvm-strip");
         var buildWorkingDir = $"openal-soft/build_android_{arch}";
         context.CreateDirectory(buildWorkingDir);
         context.CreateDirectory($"{context.ArtifactsDir}/{rid}");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DANDROID_ABI={arch} -DANDROID_PLATFORM={minNdk} -DCMAKE_TOOLCHAIN_FILE={ndk}/build/cmake/android.toolchain.cmake -DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -DALSOFT_EMBED_HRTF_DATA=TRUE -DALSOFT_REQUIRE_OPENSL=ON -DCMAKE_BUILD_TYPE=Release -DANDROID_NDK={ndk} .." });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DANDROID_ABI={arch} -DANDROID_PLATFORM={minNdk} -DCMAKE_TOOLCHAIN_FILE={ndk}/build/cmake/android.toolchain.cmake -DALSOFT_BACKEND_PIPEWIRE=OFF -DALSOFT_TESTS=OFF -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL=OFF -DALSOFT_EMBED_HRTF_DATA=TRUE -DALSOFT_REQUIRE_OPENSL=ON -DCMAKE_BUILD_TYPE=Release -DANDROID_NDK={ndk} .." });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config Release" });
+        context.StartProcess (strip, new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"--strip-all libopenal.so"});
         context.CopyFile($"{buildWorkingDir}/libopenal.so", $"{context.ArtifactsDir}/{rid}/libopenal.so");
     }
 }
